f05efc25c62c8428652ec886d2c0ff2992139eb8
MitsuhaMiyamizu <kiritoalex@live.com>
2020-10-04 02:39:12 +0000

    Revise the AppVeyor YAML

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LaptopR
 Vanilla R Compiled by mingw64 w/ Intel Math Kernel Library.
+
+**Reference**
+[R-windows](https://github.com/r-windows/r-base)
+[Zhihu](https://zhuanlan.zhihu.com/p/100847894)
+[Stackoverflow](https://stackoverflow.com/questions/38090206/linking-intels-math-kernel-library-mkl-to-r-on-windows)
--- full-build.sh
+++ full-build.sh
@@ -6,11 +6,11 @@ rm -rf src pkg
 
 # Update system
 pacman -Syyu --noconfirm
-#pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran}
+pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{icu,libtiff,libjpeg,libpng,pcre2,xz,bzip2,zlib}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{cairo,tk,curl}
-export _intel_arch=intel64
-export _intel_lib=mkl_intel_lp64
+#export _intel_arch=intel64
+#export _intel_lib=mkl_intel_lp64
 #export MKL="-L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_rt -lpthread -lm -ldl"
 #export _intel_cc_opt="-O3 -fPIC -m64 -march=native -fp-model precise -fp-model source -I${MKLROOT}/include"
 #export LDFLAGS="-qopenmp"
@@ -18,12 +18,12 @@ export _intel_lib=mkl_intel_lp64
 #export CXXFLAGS="${_intel_cc_opt}"
 #export FFLAGS="${_intel_cc_opt}"
 #export FCFLAGS="${_intel_cc_opt}"
-export CC="icc"
-export CXX="icpc"
-export F77="ifort"
-export FC="ifort"
-export AR="xiar"
-export LD="xild"
+#export CC="icc"
+#export CXX="icpc"
+#export F77="ifort"
+#export FC="ifort"
+#export AR="xiar"
+#export LD="xild"
 
 # Build package (only once)
 set -o pipefail
