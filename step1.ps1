bf391e73ad79c27140e91b8b728ef32cf141089e
G. Chen <kiritoalex@live.com>
2020-10-13 17:25:46 +0800

    Update step1.ps1

--- step1.ps1
+++ step1.ps1
@@ -1,1 +1,7 @@
-
+Import-Module .\script.ps1
+SetTimezone
+InstallInno
+InstallMiktex
+InstallRtoolsExe
+Invoke-WebRequest $env:MKL_LICENSE_FILE -OutFile $env:LICLOCAL
+C:\rtools40\usr\bin\bash --login -c "./full-build.sh"
