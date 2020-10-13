ce41aa5a1b4536970fdc80a6f2a6b1d8e922552b
G. Chen <kiritoalex@live.com>
2020-10-14 01:46:50 +0800

    Update step2.ps1

--- step2.ps1
+++ step2.ps1
@@ -1,5 +1,3 @@
-(new-object Net.WebClient).DownloadFile('http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16896/w_mkl_2020.3.279.exe','mkl.exe')
-mkl.exe --silent -a install --eula=accept --output=install.log --intel_sw_improvement_program_consent=no
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 cp -r 'C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2020.3.279\windows\mkl\tools\builder\' 'C:\projects\laptopr\'
 cp Rlapack_list.txt .\builder\
